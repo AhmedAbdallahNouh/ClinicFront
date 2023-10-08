2362b577de4944d6dcfd69df74521a1a3d8ec3ac
LAPTOP <LAPTOP@DESKTOP-L515TMB>
2023-10-08 12:47:45 +0300

    add-erquirde-password

--- ClinicModels/DTOs/MainDTO/RegiterationPasswordUserName.cs
+++ ClinicModels/DTOs/MainDTO/RegiterationPasswordUserName.cs
@@ -15,9 +15,11 @@ namespace ClinicModels.DTOs.MainDTO
         [StringLength(50)]
         public string UserName { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
